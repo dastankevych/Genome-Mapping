479e18feac2c9577493fc32fc0d5df3e2a3f2c79
Daniil Stankevych <daniil.stankevych@jetbrains.com>
2026-01-16 14:58:39 +0200

    fix 3

--- c_Burrows_Wheeler_Transform.c
+++ c_Burrows_Wheeler_Transform.c
@@ -5,25 +5,29 @@
 #define MAXN 1000005
 
 char s[MAXN];
-int p[MAXN], c[MAXN], pn[MAXN], cn[MAXN];
-int cnt[MAXN]; 
+int p_arr[MAXN], c_arr[MAXN], pn_arr[MAXN], cn_arr[MAXN];
+int cnt[MAXN];
+char out[MAXN];
 
 int main() {
+    int *p = p_arr;
+    int *c = c_arr;
+    int *pn = pn_arr;
+    int *cn = cn_arr;
+
     if (scanf("%s", s) != 1) return 0;
-    
+
     int n = strlen(s);
     s[n] = '$';
-    s[n+1] = '\0';
     n++;
+    s[n] = '\0';
 
-    memset(cnt, 0, sizeof(cnt));
+    memset(cnt, 0, 256 * sizeof(int));
 
-    for (int i = 0; i < n; ++i) cnt[(int)s[i]]++;
-    
+    for (int i = 0; i < n; ++i) cnt[(unsigned char)s[i]]++;
     for (int i = 1; i < 256; ++i) cnt[i] += cnt[i-1];
-    
-    for (int i = 0; i < n; ++i) p[--cnt[(int)s[i]]] = i;
-    
+    for (int i = 0; i < n; ++i) p[--cnt[(unsigned char)s[i]]] = i;
+
     c[p[0]] = 0;
     int classes = 1;
     for (int i = 1; i < n; ++i) {
@@ -32,44 +36,52 @@ int main() {
     }
 
     for (int h = 0; (1 << h) < n; ++h) {
+        if (classes == n) break;
+
         int val = (1 << h);
-        
+
         for (int i = 0; i < n; ++i) {
             pn[i] = p[i] - val;
             if (pn[i] < 0) pn[i] += n;
         }
-        
+
         memset(cnt, 0, classes * sizeof(int));
-        
+
         for (int i = 0; i < n; ++i) cnt[c[pn[i]]]++;
         for (int i = 1; i < classes; ++i) cnt[i] += cnt[i-1];
-        
         for (int i = n - 1; i >= 0; --i) p[--cnt[c[pn[i]]]] = pn[i];
-        
+
         cn[p[0]] = 0;
         classes = 1;
         for (int i = 1; i < n; ++i) {
             int cur = p[i];
             int prev = p[i-1];
-            
-            int mid1 = (cur + val) % n;
-            int mid2 = (prev + val) % n;
-            
+
+            int mid1 = cur + val;
+            if (mid1 >= n) mid1 -= n;
+
+            int mid2 = prev + val;
+            if (mid2 >= n) mid2 -= n;
+
             if (c[cur] != c[prev] || c[mid1] != c[mid2]) {
                 classes++;
             }
             cn[cur] = classes - 1;
         }
-        
-        memcpy(c, cn, n * sizeof(int));
+
+        int *temp = c;
+        c = cn;
+        cn = temp;
     }
 
     for (int i = 0; i < n; ++i) {
-        int idx = (p[i] - 1 + n) % n;
-        putchar(s[idx]);
+        int idx = p[i] - 1;
+        if (idx < 0) idx += n;
+        out[i] = s[idx];
     }
+
+    fwrite(out, 1, n, stdout);
     putchar('\n');
 
     return 0;
 }
-
